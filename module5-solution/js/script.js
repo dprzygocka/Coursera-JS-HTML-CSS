3e4c524d8f6892bb041e4def2365bac77b69a405
Dagmara <przygocka.dagmara@gmail.com>
2020-10-01 15:29:46 +0200

    change 10

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -101,7 +101,7 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-     var chosenCategoryShortName = '\"' + chooseRandomCategory(categories).short_name + '\"'; 
+     var chosenCategoryShortName = '\'' + chooseRandomCategory(categories).short_name + '\''; 
 
      
 
@@ -126,7 +126,8 @@ function buildAndShowHomeHTML (categories) {
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
-      insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
+      var html = insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
+      return html;
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
